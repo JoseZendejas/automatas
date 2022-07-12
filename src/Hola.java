14bd39165c6c8b28d08e2734a4295f9ee37268b8
DELL 5470 <DELL 5470@DESKTOP-HL2E5T3.mshome.net>
2022-07-12 09:55:20 -0500

    Todo

---
+++ src/Hola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL 5470
+ */
+public class Hola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal ven=new Principal();
+        ven.setVisible(true);
+       
+    }
+    
+}
